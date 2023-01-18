41155b4cf2b2036536f3dd65ed24251914e83f21
David Chosson <davidcho868@gmail.com>
2023-01-18 12:13:44 +0100

    cas nominaux 1 et 2

---
+++ exo_1.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+
+MIN=$(( $1/60 ))
+SEC=$(( $1%60 ))
+if [[ $1 > 60 ]]; then
+    echo "${MIN}m${SEC}s"
+else
+    echo "${1}s"
+fi
---
+++ exo_1_test.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# ✅
+# ❎
+
+#########################################################################
+#
+#   Fichier de test de l'exercice 1
+#
+#########################################################################
+
+# Cas nominal 1 : 5 affiche 5s
+
+echo --------------------------------------------------------------------
+echo  Test nominal 1 : 5 affiche 5s
+echo --------------------------------------------------------------------
+# Valeurs du test
+PARAMETER=5
+EXPECTED_VALUE=5s
+RECEIVED_VALUE=$(./exo_1.sh $PARAMETER)
+
+if [[ "$EXPECTED_VALUE" = "$RECEIVED_VALUE" ]]; then
+echo "✅ Test réussi"
+else
+    echo "❎ Test raté ! (valeur attendue : [$EXPECTED_VALUE], valeur reçue : [$RECEIVED_VALUE] )"
+fi
+
+# Cas nominal 2 : 75 affiche 1m15s
+
+echo --------------------------------------------------------------------
+echo  Test nominal 2 : 75 affiche 1m15s
+echo --------------------------------------------------------------------
+# Valeurs du test
+PARAMETER=75
+EXPECTED_VALUE=1m15s
+RECEIVED_VALUE=$(./exo_1.sh $PARAMETER)
+
+if [[ "$EXPECTED_VALUE" = "$RECEIVED_VALUE" ]]; then
+echo "✅ Test réussi"
+else
+    echo "❎ Test raté ! (valeur attendue : [$EXPECTED_VALUE], valeur reçue : [$RECEIVED_VALUE] )"
+fi
