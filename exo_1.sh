c01e8d8741d11b9007f6da39507771fd6f0c4eeb
David Chosson <davidcho868@gmail.com>
2023-01-18 15:52:52 +0100

    correction final cas nominal3

--- exo_1.sh
+++ exo_1.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-if [[ $# < 1 ]]; then
+if [[ $# -lt 1 ]]; then
     echo "il manque 1 paramètre"
     exit 1
     
     fi
 
-   re='^[+-]?[0-9]+([.][0-9]+)?$'
+re='^[+-]?[0-9]+([.][0-9]+)?$'
 if ! [[ ${1} =~ $re ]]; then
     echo "erreur, le paramètre n'est pas un nombre"
     exit 3
@@ -22,8 +22,12 @@ if [[ $1 -lt 0 ]]; then
 HEU=$(( $1/3600 ))
 MIN=$(( $1/60 % 60 ))
 SEC=$(( $1%60 ))
-if [[ $1 -gt 60 ]]; then
+if [[ $1 -gt 3600 ]]; then
     echo "${HEU}h${MIN}m${SEC}s"
-else
-    echo "${1}s"
+    else if [[ $1 -gt 60 ]]; then
+    echo "${MIN}m${SEC}s"
+else 
+    echo "${SEC}s"
 fi
+    fi
+        
